2563d44c82dc1842757ea2e59a712c791ea81c0d
Douglas <douglas@mouseweb.com.br>
2021-01-10 02:43:58 -0300

    DELETE cliente por ID

--- src/main/java/br/com/mouseweb/clientes/rest/ClienteController.java
+++ src/main/java/br/com/mouseweb/clientes/rest/ClienteController.java
@@ -28,4 +28,16 @@ public class ClienteController {
                 .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado") );
     }
 
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar( @PathVariable Integer id ){
+        repository
+                .findById(id)
+                .map( cliente -> {
+                    repository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado") );
+    }
+
 }
